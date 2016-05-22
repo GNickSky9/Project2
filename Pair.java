902173721afed3d349ecba06257a47a1b0b91bab
Nick Sky <GNickSky9@users.noreply.github.com>
2016-05-22 18:11:38 -0400

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,23 @@
+
+public class Pair
+{
+	private int first;
+	private int second;
+	
+	Pair(int x, int  y)
+	{
+		first = x;
+		second = y;
+	}
+
+	int getFirst()
+	{
+		return first;
+	}
+
+	int getSecond()
+	{
+		return second;
+	}
+
+}
